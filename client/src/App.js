61d661fc2054c0aba5fd0269b4a6c039911550ca
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-07-11 19:10:41 +0500

    insert Message here

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+ <Navbar/>
+    </>
+  );
+}
+
+export default App;
---
+++ db.js
@@ -0,0 +1,4 @@
+const mongoose = require("mongoose");
+
+const mongoUrl  = "mongodb+srv://saimsaeed:lofty7610@cluster0.mthnjmc.mongodb.net/";
+
