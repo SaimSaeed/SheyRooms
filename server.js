dde851d2ecefbf76a378f57ad198463da92e9eff
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-07-12 00:43:14 +0500

    routes and models created

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app  = express();
 
+const roomsRoute = require("./routes/roomsRoute")
+
+
+app.use("/api/rooms",roomsRoute)
+
+
+
 const dbConfig = require('./db');
 
 const port  =process.env.PORT || 5000;
