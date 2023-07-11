332f774721f2f879a6d588d82c892de13b8ee984
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-07-12 00:02:06 +0500

    server made

--- db.js
+++ db.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
 
-const mongoUrl  = "mongodb+srv://saimsaeed:lofty7610@cluster0.mthnjmc.mongodb.net/";
+const mongoUrl  ='mongodb+srv://saimsaeed:lofty7610@cluster0.mthnjmc.mongodb.net/mern-rooms';
 
+mongoose.connect(mongoUrl, {useUnifiedTopology: true,useNewUrlParser:true })
+
+var connection = mongoose.connection;
+
+
+connection.on('error',()=>{
+    console.log("MongoDB Connection Failed");
+})
+
+connection.on('connected',()=>{
+    console.log("MongoDB Connection Successful");
+})
+
+module.exports = mongoose;
